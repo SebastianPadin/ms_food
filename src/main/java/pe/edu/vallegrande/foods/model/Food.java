36e97dbab92c5af6c5968e7a6c6ea00b77df77e7
Sebastian Padin <126886545+SebastianPadin@users.noreply.github.com>
2025-04-24 03:51:27 +0000

    map model

--- src/main/java/pe/edu/vallegrande/foods/model/Food.java
+++ src/main/java/pe/edu/vallegrande/foods/model/Food.java
@@ -3,18 +3,34 @@ package pe.edu.vallegrande.foods.model;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.data.relational.core.mapping.Column;
 import java.time.LocalDate;
 
 @Data
 @Table("Foods")
 public class Food {
     @Id
+    @Column("id_food")
     private Long id_food;
+
+    @Column("food_type")
     private String foodType;
+
+    @Column("food_brand")
     private String foodBrand;
+
+    @Column("amount")
     private Integer amount;
+
+    @Column("packaging")
     private String packaging;
-    private String unitMeasure;  
+
+    @Column("unit_measure")
+    private String unitMeasure;
+
+    @Column("entry_date")
     private LocalDate entryDate;
+
+    @Column("status")
     private String status;
 }
