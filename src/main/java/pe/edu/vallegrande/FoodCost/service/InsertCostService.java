b2e7e6e2403c0ce44679a0edef79f6c4cf4c0361
Sebastian Padin <126886545+SebastianPadin@users.noreply.github.com>
2025-04-28 23:15:28 -0500

    Update InsertCostService.java
    hen endpoint

--- src/main/java/pe/edu/vallegrande/FoodCost/service/InsertCostService.java
+++ src/main/java/pe/edu/vallegrande/FoodCost/service/InsertCostService.java
@@ -27,7 +27,7 @@ public class InsertCostService {
     private final FoodCostsRepository foodCostsRepository;
 
     private final String foodServiceUrl = "https://msfood-production.up.railway.app/api/foods/actives";
-    private final String hensServiceUrl = "http://localhost:8085/hen/activos";
+    private final String hensServiceUrl = "https://opulent-xylophone-wp475pr44xqh9q9v-8085.app.github.dev/hen/activos";
 
     public Mono<Void> addFoodCost(FoodCostRequestDto request) {
         System.out.println("Request recibido: " + request);
