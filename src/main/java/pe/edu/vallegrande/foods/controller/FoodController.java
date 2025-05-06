fee0126e174a1265f8665f3ac2b2afb50fc20518
Sebastian Padin <126886545+SebastianPadin@users.noreply.github.com>
2025-05-06 13:14:19 -0500

    Update FoodController.java

--- src/main/java/pe/edu/vallegrande/foods/controller/FoodController.java
+++ src/main/java/pe/edu/vallegrande/foods/controller/FoodController.java
@@ -1,10 +1,7 @@
-
-/**
- * Paquete que contiene los controladores de la aplicación.
- */
 package pe.edu.vallegrande.foods.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,104 +21,55 @@ import reactor.core.publisher.Mono;
 @CrossOrigin(origins = "*")
 @RequestMapping("api/foods")
 @AllArgsConstructor
-public final class FoodController {
+public class FoodController {
 
-    /**
-     * Servicio que gestiona las operaciones sobre los alimentos.
-     */
     private final FoodService foodService;
 
-    /**
-     * Obtiene todos los alimentos disponibles en la base de datos.
-     *
-     * @return Lista de alimentos.
-     */
     @GetMapping
     public Flux<Food> getAllFoods() {
         return foodService.getAllFoods();
     }
 
-    /**
-     * Obtiene todos los alimentos activos.
-     *
-     * @return Lista de alimentos activos.
-     */
     @GetMapping("/actives")
     public Flux<Food> getAllActiveFoods() {
         return foodService.getAllActiveFoods();
     }
 
-    /**
-     * Obtiene todos los alimentos inactivos.
-     *
-     * @return Lista de alimentos inactivos.
-     */
     @GetMapping("/inactives")
     public Flux<Food> getAllInactiveFoods() {
         return foodService.getAllInactiveFoods();
     }
 
-    /**
-     * Obtiene el alimento filtrado por tipo.
-     *
-     * @param foodType Tipo de alimento
-     * @return Lista de alimentos filtrados.
-     */
     @GetMapping("/type/{foodType}")
-    public Flux<Food> getFoodsByType(@PathVariable final String foodType) {
+    public Flux<Food> getFoodsByType(@PathVariable String foodType) {
         return foodService.getFoodsByType(foodType);
     }
 
-    /**
-     * Inserta un nuevo alimento.
-     *
-     * @param request Dto que insertará el nuevo alimento
-     * @return Alimento insertado.
-     */
     @PostMapping
-    public Mono<ResponseEntity<Food>> createFood(@RequestBody final FoodRequest request) {
+    public Mono<ResponseEntity<Food>> createFood(@RequestBody FoodRequest request) {
         return foodService.createFood(request)
-            .map(food -> ResponseEntity.ok(food))
-            .defaultIfEmpty(ResponseEntity.badRequest().build());
+                .map(food -> ResponseEntity.ok(food))
+                .defaultIfEmpty(ResponseEntity.badRequest().build());
     }
 
-    /**
-     * Actualiza un nuevo alimento.
-     *
-     * @param id Identificador del alimento
-     * @param request Dto que actualizará el alimento
-     * @return Alimento actualizado.
-     */
     @PutMapping("/{id}")
-    public Mono<ResponseEntity<Food>> updateFood(@PathVariable final Long id, @RequestBody final FoodRequest foodRequest) {
+    public Mono<ResponseEntity<Food>> updateFood(@PathVariable Long id, @RequestBody FoodRequest foodRequest) {
         return foodService.updateFood(id, foodRequest)
-            .map(updatedFood -> ResponseEntity.ok(updatedFood))
-            .defaultIfEmpty(ResponseEntity.notFound().build());
+                .map(updatedFood -> ResponseEntity.ok(updatedFood))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
-    /**
-     * Elimina un alimento de manera lógica.
-     *
-     * @param id Identificador del alimento 
-     * @return Alimento eliminado.
-     */
     @PutMapping("/delete/{id}")
-    public Mono<ResponseEntity<Food>> deleteFoodLogically(@PathVariable final Long id) {
+    public Mono<ResponseEntity<Food>> deleteFoodLogically(@PathVariable Long id) {
         return foodService.deleteFoodLogically(id)
-            .map(deletedFood -> ResponseEntity.ok(deletedFood))
-            .defaultIfEmpty(ResponseEntity.notFound().build());
+                .map(deletedFood -> ResponseEntity.ok(deletedFood))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
-    /**
-     * Reestaura un alimento previamente eliminado.
-     *
-     * @param id Identificador del alimento
-     * @return Alimento restaurado.
-     */
     @PutMapping("/restore/{id}")
-    public Mono<ResponseEntity<Food>> restoreFood(@PathVariable final Long id) {
+    public Mono<ResponseEntity<Food>> restoreFood(@PathVariable Long id) {
         return foodService.restoreFood(id)
-            .map(restoredFood -> ResponseEntity.ok(restoredFood))
-            .defaultIfEmpty(ResponseEntity.notFound().build());
+                .map(restoredFood -> ResponseEntity.ok(restoredFood))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 }
