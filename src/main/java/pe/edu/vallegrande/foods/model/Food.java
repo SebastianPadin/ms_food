fcc5e3b24d5f22b72c19ceb77b8ddba168fd4d93
Sebastian Padin <126886545+SebastianPadin@users.noreply.github.com>
2025-05-06 13:11:23 -0500

    Update Food.java

--- src/main/java/pe/edu/vallegrande/foods/model/Food.java
+++ src/main/java/pe/edu/vallegrande/foods/model/Food.java
@@ -1,6 +1,3 @@
-/**
- * Paquete que contiene las entidades del modelo de datos.
- */
 package pe.edu.vallegrande.foods.model;
 
 import lombok.Data;
@@ -12,53 +9,28 @@ import java.time.LocalDate;
 @Data
 @Table("Foods")
 public class Food {
-    /**
-     * Identificador único del alimento.
-     */
     @Id
     @Column("id_food")
-    private Long idFood;
+    private Long id_food;
 
-    /**
-     * Tipo de alimento.
-     */
     @Column("food_type")
     private String foodType;
 
-    /**
-     * Marca del alimento.
-     */
     @Column("food_brand")
     private String foodBrand;
 
-    /**
-     * Cantidad disponible.
-     */
     @Column("amount")
     private Integer amount;
 
-    /**
-     * Tipo de empaque del alimento.
-     */
     @Column("packaging")
     private String packaging;
 
-    /**
-     * Unidad de medida del alimento.
-     */
     @Column("unit_measure")
     private String unitMeasure;
 
-    /**
-     * Fecha de ingreso del alimento.
-     */
     @Column("entry_date")
     private LocalDate entryDate;
 
-    /**
-     * Estado del alimento (Activo/Inactivo).
-     */
     @Column("status")
     private String status;
-
 }
