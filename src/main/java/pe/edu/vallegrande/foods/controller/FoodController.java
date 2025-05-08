8debc410e2383f7f219a6b0eddcabf5512a89aca
Sebastian Padin <126886545+SebastianPadin@users.noreply.github.com>
2025-05-08 03:57:59 +0000

    run

--- src/main/java/pe/edu/vallegrande/foods/controller/FoodController.java
+++ src/main/java/pe/edu/vallegrande/foods/controller/FoodController.java
@@ -1,7 +1,6 @@
 package pe.edu.vallegrande.foods.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
