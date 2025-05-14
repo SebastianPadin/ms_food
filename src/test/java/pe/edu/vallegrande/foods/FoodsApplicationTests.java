a5049e8ad8c92e006dee688612a10c0d5523de11
Sebastian Padin <126886545+SebastianPadin@users.noreply.github.com>
2025-05-13 21:08:26 -0500

    Update FoodsApplicationTests.java

--- src/test/java/pe/edu/vallegrande/foods/FoodsApplicationTests.java
+++ src/test/java/pe/edu/vallegrande/foods/FoodsApplicationTests.java
@@ -3,7 +3,7 @@ package pe.edu.vallegrande.foods;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.r2dbc.enabled=false"})
 class FoodsApplicationTests {
 
 	@Test
