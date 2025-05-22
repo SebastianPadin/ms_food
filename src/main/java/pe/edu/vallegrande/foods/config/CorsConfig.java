4177cd47ffe4a3932d5672e6872428f51d51199f
Sebastian Padin <126886545+SebastianPadin@users.noreply.github.com>
2025-05-22 12:42:30 -0500

    Update CorsConfig.java

--- src/main/java/pe/edu/vallegrande/foods/config/CorsConfig.java
+++ src/main/java/pe/edu/vallegrande/foods/config/CorsConfig.java
@@ -21,7 +21,7 @@ public class CorsConfig {
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.addAllowedOrigin(
-            "http://localhost:4200"
+            "https://4200-vallegrande-vgwebdashbo-ya5bwcbfphb.ws-us118.gitpod.io"
         );
         corsConfig.addAllowedMethod("*");
         corsConfig.addAllowedHeader("*");
