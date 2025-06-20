e39b3f0e05877170ca87a87a9b340b8c3bc54b4c
Sebastian <sebastian.padin@vallegrande.edu.pe>
2025-06-20 02:36:08 -0500

    seguridad

--- src/main/java/pe/edu/vallegrande/foodcost/webclient/config/WebClientConfig.java
+++ src/main/java/pe/edu/vallegrande/foodcost/webclient/config/WebClientConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -19,6 +20,7 @@ public class WebClientConfig {
         return builder
                 .baseUrl(foodServiceUrl)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .filter(jwtPropagationFilter())                      // Propagación del token
                 .filter(errorHandlingFilterForFood())
                 .build();
     }
@@ -28,8 +30,23 @@ public class WebClientConfig {
         return builder
                 .baseUrl(hensServiceUrl)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .filter(jwtPropagationFilter())                      // Propagación del token
                 .filter(errorHandlingFilterForHens())
                 .build();
+    }
+
+    private ExchangeFilterFunction jwtPropagationFilter() {
+        return (request, next) -> Mono.deferContextual(ctx -> {
+            if (ctx.hasKey("Authorization")) {
+                String token = ctx.get("Authorization");
+                return next.exchange(
+                        ClientRequest.from(request)
+                                .headers(headers -> headers.setBearerAuth(token))
+                                .build()
+                );
+            }
+            return next.exchange(request);
+        });
     }
 
     private ExchangeFilterFunction errorHandlingFilterForFood() {
