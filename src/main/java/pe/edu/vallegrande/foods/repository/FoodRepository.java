c2c36a25e82d4ccf41e6de176fc6694a6222e8b5
Sebastian <sebastian.padin@vallegrande.edu.pe>
2025-06-25 15:50:44 -0500

    mejora de busqueda

--- src/main/java/pe/edu/vallegrande/foods/repository/FoodRepository.java
+++ src/main/java/pe/edu/vallegrande/foods/repository/FoodRepository.java
@@ -1,7 +1,6 @@
 package pe.edu.vallegrande.foods.repository;
 
 import org.springframework.data.r2dbc.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import pe.edu.vallegrande.foods.model.Food;
 import org.springframework.stereotype.Repository;
@@ -11,6 +10,7 @@ import reactor.core.publisher.Flux;
 public interface FoodRepository extends ReactiveCrudRepository<Food, Long> {
     Flux<Food> findAllByStatus(String status);
 
-    @Query("SELECT * FROM Foods WHERE LOWER(food_type) LIKE LOWER(CONCAT('%', :foodType, '%')) AND status = 'A'")
-    Flux<Food> findByFoodTypeContaining(@Param("foodType") String foodType);
+    @Query("SELECT * FROM Foods WHERE LOWER(food_type) LIKE LOWER(CONCAT('%', ?, '%')) AND status = 'A'")
+    Flux<Food> findByFoodTypeContaining(String foodType);
+
 }
